dedbf76da5fe0edd774483c00d1adda9f0a9c528
Tarctic <zainabshah2003@gmail.com>
2023-02-09 18:24:21 +0300

    Add davinci API chatbot

---
+++ app.py
@@ -0,0 +1,38 @@
+# import nltk
+from nltk.chat.util import Chat, reflections
+from flask import Flask, request, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get", methods = ["GET"])
+def get_bot_response():
+    user_message = request.args.get('msg')
+    api_key = ""
+    model = "text-davinci-003"
+    response = generate_response_gpt3(user_message, model, api_key)
+    return response
+
+def generate_response_gpt3(user_message, model, api_key):
+    prompt = (f"User: {user_message}\n"
+              f"Chatbot: ")
+    response = requests.post(
+        "https://api.openai.com/v1/engines/text-davinci-003/completions",
+        headers = {"Authorization" : f"Bearer {api_key}"},
+        json={
+            "prompt" : prompt,
+            "max_tokens" : 200,
+            "temperature": 0.7
+        },
+    )
+
+    return response.json()["choices"][0]["text"].strip()
+
+
+if __name__ == "__main__":
+    app.run()
