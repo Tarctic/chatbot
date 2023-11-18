aaf1f5b96be3bd6e43eb95705b51dfc66a10419d
Tarctic <zainabshah2003@gmail.com>
2023-11-18 03:06:27 +0300

    create caps bot function

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-# import nltk
-from nltk.chat.util import Chat, reflections
 from flask import Flask, request, render_template
-import requests
-import json
 
 app = Flask(__name__)
 
@@ -13,27 +9,11 @@ def index():
 @app.route("/get", methods = ["GET"])
 def get_bot_response():
     user_message = request.args.get('msg')
-    api_key = ""
-    model = "text-davinci-003"
-    response = generate_response_gpt3(user_message, model, api_key)
+    response = generate_response_CAPS(user_message)
     return response
 
-def generate_response_gpt3(user_message, model, api_key):
-    prompt = (f"User: {user_message}\n"
-              f"Chatbot: ")
-    response = requests.post(
-        f"https://api.openai.com/v1/engines/{model}/completions",
-        headers = {"Authorization" : f"Bearer {api_key}"},
-        json={
-            "prompt" : prompt,
-            "max_tokens" : 200,
-            "temperature": 0.7
-        },
-    )
-
-    reply = response.json()["choices"][0]["text"].strip()
-    return reply
-
+def generate_response_CAPS(user_message):
+    return user_message.upper()
 
 if __name__ == "__main__":
     app.run()
